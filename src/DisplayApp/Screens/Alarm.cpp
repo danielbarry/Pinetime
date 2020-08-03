da5d06c885f966fbe6fcbe922b9e87b9d9ea0ea5
unknown <anirudhadiraju@gmail.com>
2020-08-03 01:36:05 -0500

    bug fix

--- src/DisplayApp/Screens/Alarm.cpp
+++ src/DisplayApp/Screens/Alarm.cpp
@@ -222,7 +222,7 @@ Alarm::Alarm(Pinetime::Applications::DisplayApp* app, Controllers::VibrationMoto
     lv_obj_set_width(dd, LV_DPI * 2);
     lv_obj_set_height(dd, 50);
     lv_ddlist_set_options(dd, "January\nFebruary\nMarch\nApril\nMay\nJune\nJuly\nAugust\nSeptember\nOctober\nNovember\nDecember");
-    //lv_obj_set_event_cb(dd, dd_change);
+    lv_obj_set_event_cb(dd, dd_change);
     lv_ddlist_set_fix_height(dd, 135);
     lv_obj_align(dd, NULL, LV_ALIGN_IN_TOP_MID, 0, 25);
     
